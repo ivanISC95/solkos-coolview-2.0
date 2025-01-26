5759ede9f1b71864f47104983589b9f18b7b1725
Jose Ivan Perez <ivan2295isc@gmail.com>
2025-01-25 18:45:41 -0600

    Add new organization

--- src/app/Main/SelectMain/SelectMain.component.ts
+++ src/app/Main/SelectMain/SelectMain.component.ts
@@ -1,13 +1,22 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { SelectComponent } from "../../Components/Select/Select.component";
 import { GraphViewComponent } from "../../Components/GraphView/GraphView.component";
+import { FormsModule } from '@angular/forms';
 
+
+import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
 @Component({
   selector: 'app-select-main',
   standalone: true,
-  imports: [SelectComponent, GraphViewComponent],
+  imports: [SelectComponent, GraphViewComponent,FormsModule,NzDatePickerModule],
   templateUrl: './SelectMain.component.html',
   styleUrl: './SelectMain.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SelectMainComponent { }
+export class SelectMainComponent { 
+  date = null;
+
+  onChange(result: Date[]): void {
+    console.log('onChange: ', result);
+  }    
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SelectMainComponent } from "./Main/SelectMain/SelectMain.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SelectMainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
