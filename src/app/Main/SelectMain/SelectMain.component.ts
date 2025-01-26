2713663478dc249bf23118c220c6b97cc6bca7be
Jose Ivan Perez <ivan2295isc@gmail.com>
2025-01-25 19:20:03 -0600

    Update UI  Add Select
    Update UI , Add select

--- src/app/Main/SelectMain/SelectMain.component.ts
+++ src/app/Main/SelectMain/SelectMain.component.ts
@@ -1,6 +1,8 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { SelectComponent } from "../../Components/Select/Select.component";
 import { GraphViewComponent } from "../../Components/GraphView/GraphView.component";
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzInputModule } from 'ng-zorro-antd/input';
 import { FormsModule } from '@angular/forms';
 
 
@@ -8,13 +10,14 @@ import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
 @Component({
   selector: 'app-select-main',
   standalone: true,
-  imports: [SelectComponent, GraphViewComponent,FormsModule,NzDatePickerModule],
+  imports: [SelectComponent, GraphViewComponent,FormsModule,NzDatePickerModule,NzButtonModule,NzInputModule],
   templateUrl: './SelectMain.component.html',
   styleUrl: './SelectMain.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SelectMainComponent { 
   date = null;
+  value = ''
 
   onChange(result: Date[]): void {
     console.log('onChange: ', result);
