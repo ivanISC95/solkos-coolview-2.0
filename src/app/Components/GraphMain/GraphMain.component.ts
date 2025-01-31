28fc4be25ba50e41b0a5c02fb901c6cf479023fd
ivanISC95 <79552848+ivanISC95@users.noreply.github.com>
2025-01-31 13:30:44 -0600

    Update UI
    Update UI into graph zone

--- src/app/Components/GraphMain/GraphMain.component.ts
+++ src/app/Components/GraphMain/GraphMain.component.ts
@@ -1,12 +1,14 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { NzSelectModule } from 'ng-zorro-antd/select';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-graph-main',
   standalone: true,
-  imports: [NzSelectModule],
+  imports: [NzSelectModule,CommonModule],
   templateUrl: './GraphMain.component.html',
   styleUrl: './GraphMain.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class GraphMainComponent { }
+export class GraphMainComponent {  
+ }
