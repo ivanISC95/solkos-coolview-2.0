9c7be785888a957273031230f923774dae17b3db
ivanISC95 <ivan2295ISC@gmail.com>
2025-02-01 01:15:22 -0600

    Add drawer
    Add drawer

--- src/app/Components/GraphMain/GraphMain.component.ts
+++ src/app/Components/GraphMain/GraphMain.component.ts
@@ -1,14 +1,21 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { NzSelectModule } from 'ng-zorro-antd/select';
 import { CommonModule } from '@angular/common';
+import { NzDrawerModule } from 'ng-zorro-antd/drawer';
+
 
 @Component({
   selector: 'app-graph-main',
   standalone: true,
-  imports: [NzSelectModule,CommonModule],
+  imports: [NzSelectModule,CommonModule,NzDrawerModule],
   templateUrl: './GraphMain.component.html',
   styleUrl: './GraphMain.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class GraphMainComponent {  
+  drawer_status : boolean = false; 
+
+  close() {    
+    this.drawer_status = false
+  }
  }
