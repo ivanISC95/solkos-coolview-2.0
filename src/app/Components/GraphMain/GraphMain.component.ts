0f8dd566155dea506007fb952419cd38920038f4
ivanISC95 <ivan2295ISC@gmail.com>
2025-02-01 21:18:19 -0600

    Update Drawer UI
    Update Drawer UI

--- src/app/Components/GraphMain/GraphMain.component.ts
+++ src/app/Components/GraphMain/GraphMain.component.ts
@@ -2,20 +2,28 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { NzSelectModule } from 'ng-zorro-antd/select';
 import { CommonModule } from '@angular/common';
 import { NzDrawerModule } from 'ng-zorro-antd/drawer';
-
+import { NzFlexDirective } from 'ng-zorro-antd/flex';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+import { FormsModule } from '@angular/forms';
+import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
 
 @Component({
   selector: 'app-graph-main',
   standalone: true,
-  imports: [NzSelectModule,CommonModule,NzDrawerModule],
+  imports: [NzSelectModule,CommonModule,NzDrawerModule,NzFlexDirective,NzCheckboxModule,FormsModule,NzDatePickerModule],
   templateUrl: './GraphMain.component.html',
   styleUrl: './GraphMain.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class GraphMainComponent {  
   drawer_status : boolean = false; 
+  checked = true;
+  date : null | Date[] = null;
 
   close() {    
     this.drawer_status = false
   }
+  onChange(result: Date[]): void {
+    this.date = result    
+  }  
  }
