73b9b16d88d0df59cdf406d0da841b38f4c05643
ivanISC95 <79552848+ivanISC95@users.noreply.github.com>
2025-02-06 13:47:01 -0600

    Update Code clean code
    Clean code

--- src/app/Components/GraphMain/GraphMain.component.ts
+++ src/app/Components/GraphMain/GraphMain.component.ts
@@ -8,6 +8,7 @@ import { FormsModule } from '@angular/forms';
 import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
 import { DatasResponse } from '../../DatasResponse';
 import { getTelemetryNamesTranslated, transformTelemetry2 } from '../../Functions/GraphFunctions';
+import { graph_config, graph_layout } from '../../Functions/GraphVar';
 
 @Component({
   selector: 'app-graph-main',
@@ -41,62 +42,8 @@ export class GraphMainComponent implements OnInit {
   }
   basicChart(data_graph:any) {
     const element = this.el().nativeElement
-    const data = data_graph;
-    const layout = {      
-      autosize: true,
-      showlegend: true,
-      plot_bgcolor: '#f8f9fa',
-      paper_bgcolor: '#f8f9fa',
-      hovermode: 'x',
-      font: {
-        family: 'DM Mono',
-        size: 12,
-        color: '#868E96',
-      },
-      legend: {
-        x: 0.95, // Mueve la leyenda más hacia la izquierda
-        xanchor: 'right',
-        yanchor: 'bottom',
-        y: 0.95,
-        orientation: 'h',
-        font: {
-          family: 'DM Sans, monospace',
-          color: '#495057',
-        },
-        traceorder: 'normal',
-      },
-      xaxis: {
-        tickformat: '%d-%b',
-        showgrid: false,
-        type: 'date'
-        // range: rangos.length > 0 ? [rangos[0], rangos[1]] : undefined
-      },
-      yaxis: {
-        autorange: true,
-        tickformat: '~s',
-        // ticksuffix: LayoutInforTelemetry(value)[1],
-        zeroline: false,
-        showgrid: true,
-        tickfont: {
-          family: 'DM Mono',
-          size: 12,
-          color: '#868E96'
-        },
-      },
-      margin: {
-        t: 10,
-        b: 30,
-        l: 30,
-        r: 30
-      }
-    };
-    const config = {
-      responsive: true,
-      displayModeBar: true,
-      modeBarButtonsToRemove: ['pan2d', 'select2d', 'lasso2d', 'autoScale2d'], displaylogo: false
-    };
-
-    Plotly.newPlot(element, data, layout, config);
+    const data = data_graph;     
+    Plotly.newPlot(element, data, graph_layout(), graph_config);
   }
   resizeChart() {
     const element = this.el().nativeElement;
