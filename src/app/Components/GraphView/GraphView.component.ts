ba27b5c5859b7aa407e77895cae4ce514d5149f4
ivanISC95 <79552848+ivanISC95@users.noreply.github.com>
2025-11-03 13:41:14 -0600

    Update select component
    Update UI , se agrega icono y titulo de coolview , se actualizan cuadros de visualizacion junto con colores de background ,se añade nuevo background en caso que este seleccinada la opcion

--- src/app/Components/GraphView/GraphView.component.ts
+++ src/app/Components/GraphView/GraphView.component.ts
@@ -1,17 +1,20 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-graph-view',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './GraphView.component.html',
   styleUrl: './GraphView.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class GraphViewComponent { 
   @Output() mensajeEvento = new EventEmitter<number>();
+  select_opt : number = 1  
   
   shareViewOption(value:number){
     this.mensajeEvento.emit(value)
+    this.select_opt = value
   }
 }
