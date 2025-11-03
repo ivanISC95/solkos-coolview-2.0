e46a27be0f2c30bb07cdab68fb8a6055a2c6a5e4
Jose Ivan Perez <ivan2295isc@gmail.com>
2025-11-03 15:53:02 -0600

    update routing !IMPORTANT
    Update routing , add new routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,25 @@
+// import { Routes } from '@angular/router';
+// import { ConsoleMainComponent } from './Main/ConsoleMain/ConsoleMain.component';
+// import { SelectMainComponent } from './Main/SelectMain/SelectMain.component';
+
+// export const routes: Routes = [    
+//     { path: 'coolview/:id', component: ConsoleMainComponent }, // primero
+//     // { path: '**', component: SelectMainComponent }, 
+// ];
+
 import { Routes } from '@angular/router';
+import { UrlSegment, UrlMatchResult } from '@angular/router';
 import { ConsoleMainComponent } from './Main/ConsoleMain/ConsoleMain.component';
 import { SelectMainComponent } from './Main/SelectMain/SelectMain.component';
 
-export const routes: Routes = [    
-    { path: 'Coolview/:id', component: ConsoleMainComponent }, // primero
-    { path: '**', component: SelectMainComponent }, 
+export function caseInsensitiveMatcher(url: UrlSegment[]): UrlMatchResult | null {
+  if (url.length === 2 && url[0].path.toLowerCase() === 'coolview') {
+    return { consumed: url, posParams: { id: url[1] } };
+  }
+  return null;
+}
+
+export const routes: Routes = [
+  { matcher: caseInsensitiveMatcher, component: ConsoleMainComponent },
+  { path: '**', component: SelectMainComponent },
 ];
